cbf684d82bf72d0ad3ac221750e2061cba8e7ce2
Jeremy <jeremydog1@gmail.com>
2017-03-04 12:25:32 +1300

    Implement basic login logic

--- php/login.php
+++ php/login.php
@@ -1,6 +1,33 @@
 <?php
 
-echo "The server responds";
+$username = $_GET['user'];
+$password = $_GET['pass'];
+
+//Connect to database
+$con = mysqli_connect("localhost","root","root", "333A1");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$query = "SELECT * 
+          FROM Users 
+          WHERE username = '$username'
+          AND binary password = '$password'"; //Use binary for case sensitivity
+          
+$result = mysqli_query($con, $query);
+$row = mysqli_fetch_assoc($result);
+$count = mysqli_num_rows($result);
+
+if ($count == 1) {
+    echo "it worked";
+}
+else {
+    echo "Incorrect username or password";
+}
+
+
 
 
 
