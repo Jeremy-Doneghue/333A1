fac4ed0edc45923041f2aff26bde730a99ae16ee
Jeremy <jeremydog1@gmail.com>
2017-03-04 16:18:58 +1300

    Script now returns json of username, favourite stocks and the stock information for each

--- php/login.php
+++ php/login.php
@@ -21,14 +21,42 @@ $row = mysqli_fetch_assoc($result);
 $count = mysqli_num_rows($result);
 
 if ($count == 1) {
-    echo "it worked";
+    sendData($con, $row['uid'], $row['username']);
 }
 else {
     echo "Incorrect username or password";
 }
 
+//Close the connection
+mysqli_close($con);
 
+function sendData($con, $id, $username) {
 
+    //Get the user's favourite stocks
+    $favStocksQuery = "SELECT stock FROM FavouriteStocks WHERE user = '$id'";
+    $favStockResult = mysqli_query($con, $favStocksQuery);
+    $numrows = mysqli_num_rows($favStockResult);
+    $stocks = array();
+    while ($id = mysqli_fetch_assoc($favStockResult)['stock']) {
 
+        //Query each stock
+        $stockQuery = "SELECT * FROM stocks WHERE id = '$id'";
+        $result = mysqli_query($con, $stockQuery);
+        $row = mysqli_fetch_assoc($result);
 
+        //Put the info into an object
+        $stockObject = (object)[
+            'companyname' => $row['companyname'],
+            'currentprice' => $row['currentprice'],
+            'recentchange' => $row['recentchange'],
+            'annualtrend' => $row['annualtrend'],
+            'recentchangedirection' => $row['recentchangedirection']            
+        ];
+        array_push($stocks, $stockObject);
+    }
+    
+    // Object to encode to json
+    $user = (object)['name' => $username, 'favStocks' => $stocks];
+    echo json_encode($user);
+}
 ?>
