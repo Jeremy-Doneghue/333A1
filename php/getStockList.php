ad3cbedbf82019f041b154c7c852275f05a06d74
Jeremy <jeremydog1@gmail.com>
2017-03-06 13:52:14 +1300

    Add ajax call to get full list of stocks

---
+++ php/getStockList.php
@@ -0,0 +1,32 @@
+<?php
+    //Connect to database
+    $con = mysqli_connect("localhost","root","root", "333A1");
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $query = "SELECT * FROM stocks";
+    $result = mysqli_query($con, $query);
+
+    $stocks = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        //Put the info into an object
+        $stockObject = (object)[
+            'companyname' => $row['companyname'],
+            'currentprice' => $row['currentprice'],
+            'recentchange' => $row['recentchange'],
+            'annualtrend' => $row['annualtrend'],
+            'recentchangedirection' => $row['recentchangedirection']            
+        ];
+        array_push($stocks, $stockObject);
+    }
+
+    //Echo the result
+    echo json_encode($stocks);
+           
+    //Close the connection
+    mysqli_close($con);
+?>
--- scripts/session.js
+++ scripts/session.js
@@ -1,4 +1,5 @@
 var user = {};
+var fullStockList = {};
 
 /**
  * Returns whether the user us logged in or not
@@ -12,10 +13,27 @@ function userLoggedIn() {
 
 function setUser(jsonString) {
     user = Object.assign(user, JSON.parse(jsonString));
+    retrieveFullStockList();
     rvc.render();
 }
 
 function logOut(){
     user = {};
     rvc.render();
+}
+
+/**
+ * Retrieve a full list of stocks in json format
+ */
+function retrieveFullStockList() {
+
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {            
+            //console.log(this.responseText);
+            console.log(JSON.stringify(JSON.parse(this.responseText), null, 2));
+        }
+    };
+    ajax.open("GET", `php/getStockList.php`);
+    ajax.send();
 }
